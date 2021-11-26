fb527aa5e8604d5d604a645764d50083d03b18d1
Nikita Tulenev <n1tulenev@gmail.com>
2021-11-26 13:53:27 +0300

    Move to record structs

--- src/HostedServiceMediatRExample/Models/RequestId.cs
+++ src/HostedServiceMediatRExample/Models/RequestId.cs
@@ -1,36 +1,4 @@
 ﻿namespace HostedServiceMediatRExample.Models
 {
-    public struct RequestId : IEquatable<RequestId>
-    {
-        public long Id { get; }
-
-        public RequestId(long id)
-        {
-            Id = id;
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (obj is RequestId requestId)
-            {
-                return Equals(requestId);
-            }
-
-            return false;
-        }
-        public override int GetHashCode() => HashCode.Combine(Id);
-
-        public bool Equals(RequestId other)
-            => Id == other.Id;
-
-        public static bool operator ==(RequestId left, RequestId right)
-        {
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(RequestId left, RequestId right)
-        {
-            return !(left == right);
-        }
-    }
+    public readonly record struct RequestId(long Id);
 }
